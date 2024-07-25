315bf938d262dc6db89518ca3ad908d6fc5304c1
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-25 15:41:46 +0200

    feat: move the logic in another file and include it in the index.php with require

--- index.php
+++ index.php
@@ -1,37 +1,7 @@
 <?php
-// creo 4 array diversi per lettere minuscolo, lettere maiuscolo numeri e caratteri speciali
-$lowerCase = range('a', 'z');
-$upperCase = range('A', 'Z');
-$numbers = range(0, 9);
-$specials = str_split('!?&%$<>^+-*/()[]{}@#_='); //crea un array con la stringa passata
 
-$elements = array_merge($lowerCase, $upperCase, $numbers, $specials); //unisco tutti gli array in uno unico
-
-// $lunghezza = count($elements);
-// var_dump($_GET['lunghezza']);
-// die();
-
-// scrittura della funzione che prende il numero inserito dalla persona -> se è compreso tra 8 e 32 allora genera una psw
-function generatePsw($number){
-    // per accedere al mio array generato globalmente
-    global $elements;
-    if($number >= 8 && $number <= 32){
-        // creo una stringa vuota
-        $pswElement = '';
-
-        // finchè i è minore del numero prende un elemento random dell'array e lo inserisce nella stringa
-        for($i = 0; $i < $number; $i++){
-            // prendere elemento random dall'array
-            $element = $elements[rand(0, count($elements)-1)];
-            // inserirlo nella stringa
-            $pswElement .= $element;
-        }  
-    } else {
-        $pswElement = 'Errore. Devi generare una password di lunghezza compresa fra 8 e 32';
-    }
-
-    return $pswElement;
-}
+// includo il file della funzione creata
+require_once __DIR__ . '/functions.php';
 
 
 // se il numero che ha inserito è registrato e non è vuoto allora fai la funzione
