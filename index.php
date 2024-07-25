b29eb4c73d07e4aab010877bf8038752c26f1fc0
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-25 13:51:59 +0200

    build: add base project file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// prende l'elemento inserito dalla persona
+
+// scrittura della funzione che riprende l'elemento inserito
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+
+<!-- scrittura del form per scegliere il numeri dei caratteri da 8 a 32 -->
+    
+</body>
+</html>
